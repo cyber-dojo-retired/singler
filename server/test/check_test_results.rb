d1678ea30202e2a0e47595b22cb3f8004ce14e1c
Jon Jagger <jon@jaggersoft.com>
2018-10-06 14:46:22 +0100

    refactoring

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -98,8 +98,8 @@ table =
     [ 'duration(test)[s]',      test_duration,      '<=',   2 ],
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
-    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 1.6 ],
-    [ 'hits(src)/hits(test)',   f2(hits_ratio),     '>=',  16 ],
+    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 1.5 ],
+    [ 'hits(src)/hits(test)',   f2(hits_ratio),     '>=',  15 ],
   ]
 
 # - - - - - - - - - - - - - - - - - - - - - - -
